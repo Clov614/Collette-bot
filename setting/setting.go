a141a25834328cd782cf5218406b9055f8398a88
Clov614 <541982545@qq.com>
2022-06-12 15:46:53 +0800

    fix: 让退出更加优雅

--- setting/setting.go
+++ setting/setting.go
@@ -29,14 +29,14 @@ func init() {
 		log.Info("生成默认setting,yml成功")
 		log.Info("请配置setting.yml后重启程序...")
 		time.Sleep(time.Second * 5)
-		log.Fatalln()
+		os.Exit(0)
 	}
 	ReadYaml(&Data, "./setting.yml")
 	if len(Data.Nickname) == 0 {
 		log.Info("未配置setting.yml")
 		log.Info("Nickname为空")
 		time.Sleep(time.Second * 5)
-		log.Fatalln()
+		os.Exit(0)
 	}
 
 }
@@ -56,23 +56,23 @@ func WriteYaml(_type interface{}, path string) {
 	//dataStr为[]byte,准备写入yaml
 	dataStr, err := yaml.Marshal(_type)
 	if err != nil {
-		log.Fatalln("WriteYaml() Error: ", err)
+		log.Error("WriteYaml() Error: ", err)
 	}
 
 	err = ioutil.WriteFile(path, dataStr, 0644)
 	if err != nil {
-		log.Fatalln("WriteYaml() writeFile Error path: "+path, err)
+		log.Error("WriteYaml() writeFile Error path: "+path, err)
 	}
 }
 
 func ReadYaml(_type interface{}, path string) {
 	file, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalln("读取Error path: "+path, err)
+		log.Error("读取Error path: "+path, err)
 	}
 	err = yaml.Unmarshal(file, _type)
 	if err != nil {
-		log.Fatalln("ERROR:"+path+" to data error: ", err)
+		log.Error("ERROR:"+path+" to data error: ", err)
 	}
 }
 
@@ -80,12 +80,12 @@ func WriteYamlAppend(_type interface{}, path string) {
 	//dataStr为[]byte,准备写入yaml
 	dataStr, err := yaml.Marshal(_type)
 	if err != nil {
-		log.Fatalln("WriteYaml() Error: ", err)
+		log.Error("WriteYaml() Error: ", err)
 	}
 
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		log.Fatalln("WriteYaml() writeFile Error path: "+path, err)
+		log.Error("WriteYaml() writeFile Error path: "+path, err)
 	}
 	helpStr := "# Nickname为机器人昵称\n# SelfQQ为机器人QQ号\n"
 	file.Write([]byte(helpStr))
